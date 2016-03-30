5fa6b448dc4f7d9f052a1f6a011fc4e91b4ef416
subramanyamduvvuri <duvvurisubramanyam@gmail.com>
2016-03-30 22:17:20 +0200

    Dennise ritche problem Horizontal and vertical histogram has been implemented

--- Rexe1-13.c
+++ Rexe1-13.c
@@ -2,16 +2,19 @@
 //Page number 24,25 of Dennise Ritche
 //exercise 1-13 also included
 //Horizontal histogram has been made
+//Vertical histogram implemented
 #include<stdio.h>
 #define MAX 10
 int main()
 {
 	int c ;
 	int ndigit[MAX];
-	int i,j;
+	int ncheck[MAX];
+	int i,j,temp=0;
 	for ( i=0; i <MAX;i++)
 	{
 		ndigit[i]=0;
+		ncheck[i]=0;
 	}
 	
 	while((c=getchar())!= EOF)
@@ -21,7 +24,7 @@ int main()
 		
 	}
 	
-	for ( i=0; i <MAX;i++)
+	for ( i=0; i <MAX;i++)				//Horizontal histogram
 	{
 		printf("%d-->%d",i,ndigit[i]);
 		for(j=0;j<ndigit[i];j++)
@@ -31,7 +34,62 @@ int main()
 		printf("\n");
 				
 	}
-	
-	return 0;
 
+	printf("\n------------------\n");
+	//Algorithm to find the maximumnumber in an array
+	for(i=0;i<MAX;i++)
+	{
+		if (temp<ndigit[i])
+		{
+			temp=ndigit[i];
+		}
+	}		
+	printf("%d",temp);
+
+	printf("\n------------------\n");
+	
+
+	
+	for ( i=0; i<MAX ; i++)
+		printf("%d\t",i);
+
+	printf ("\n");
+	
+	for ( i=0; i<MAX ; i++)
+		printf("|\t");
+
+	
+	printf("\n");	
+	
+	for (i=0; i<MAX ; i++)
+		printf ("%d\t",ndigit[i]);
+	
+	printf("\n");
+
+
+
+	
+	for(j=0;j<temp;j++)
+	{
+
+		for ( i=0;i<MAX;i++)
+		{
+			if (ncheck[i]!=ndigit[i])
+			{
+				printf("||\t");
+				++ncheck[i];
+			}
+			else 
+			{
+				printf("\t");
+			}
+		
+			
+			
+		}
+			printf("\n");
+
+
+	}	
+	return 0;
 }
